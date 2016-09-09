9d3fd1a1334d854f03e6be16b776a3c5c22eef13
hymanme <hymanme@163.com>
2016-09-10 00:43:03 +0800

    readme

--- README.md
+++ README.md
@@ -3,6 +3,9 @@ An Android Material Design Demo
 
 ![book item](http://ww3.sinaimg.cn/mw690/005X6W83gw1f6p032la2tj30dw04vq4g.jpg)
 
+##鸣谢
+    - 粒子效果启动页[ParticleView](https://github.com/JeasonWong/Particle)
+
 ##效果图：
 
 ![list](http://ww4.sinaimg.cn/mw690/005X6W83gw1f6p0339jkbj30c00lcn3f.jpg)
--- app/src/main/java/com/hymane/materialhome/bean/TagBean.java
+++ app/src/main/java/com/hymane/materialhome/bean/TagBean.java
@@ -1,12 +1,15 @@
 package com.hymane.materialhome.bean;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Author   :hymanme
  * Email    :hymanme@163.com
  * Create at 2016/1/8
  * Description:
  */
-public class TagBean {
+public class TagBean implements Parcelable{
     private int count;
     private String name;
     private String title;
@@ -34,4 +37,34 @@ public class TagBean {
     public void setTitle(String title) {
         this.title = title;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(count);
+        dest.writeString(name);
+        dest.writeString(title);
+    }
+
+    protected TagBean(Parcel in) {
+        count = in.readInt();
+        name = in.readString();
+        title = in.readString();
+    }
+
+    public static final Creator<TagBean> CREATOR = new Creator<TagBean>() {
+        @Override
+        public TagBean createFromParcel(Parcel in) {
+            return new TagBean(in);
+        }
+
+        @Override
+        public TagBean[] newArray(int size) {
+            return new TagBean[size];
+        }
+    };
 }
