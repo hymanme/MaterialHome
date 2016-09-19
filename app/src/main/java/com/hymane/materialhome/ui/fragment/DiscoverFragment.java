02f8335354a20a442c60a3d582bec45a30b88012
hymane <hymanme@163.com>
2016-09-19 18:02:13 +0800

    加入发现页面(热门搜索待实现) 优化发现页面换肤适配

--- app/src/main/java/com/hymane/materialhome/ui/fragment/DiscoverFragment.java
+++ app/src/main/java/com/hymane/materialhome/ui/fragment/DiscoverFragment.java
@@ -37,7 +37,7 @@ import butterknife.ButterKnife;
 /**
  * Author   :hymanme
  * Email    :hymanme@163.com
- * Create at 2016/1/14 0014
+ * Create at 2016/9/14
  * Description:
  */
 public class DiscoverFragment extends BaseFragment implements IHotSearchView {
